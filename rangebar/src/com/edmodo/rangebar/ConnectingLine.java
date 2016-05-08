4dea966328bcc0676d913dc1f997bebcad4456e2
Alexander Bezverhni <bezverhni@gmail.com>
2016-05-08 14:12:15 +0300

    Code format. Zorro code changes.

--- rangebar/src/com/edmodo/rangebar/ConnectingLine.java
+++ rangebar/src/com/edmodo/rangebar/ConnectingLine.java
@@ -24,42 +24,43 @@ import android.util.TypedValue;
  */
 class ConnectingLine {
 
-    // Member Variables ////////////////////////////////////////////////////////
+	// Member Variables ////////////////////////////////////////////////////////
 
-    private final Paint mPaint;
+	private final Paint mPaint;
 
-    private final float mConnectingLineWeight;
-    private final float mY;
+	private final float mConnectingLineWeight;
+	private final float mY;
 
-    // Constructor /////////////////////////////////////////////////////////////
+	// Constructor /////////////////////////////////////////////////////////////
 
-    ConnectingLine(Context ctx, float y, float connectingLineWeight, int connectingLineColor) {
+	ConnectingLine(Context ctx, float y, float connectingLineWeight, int connectingLineColor) {
 
-        final Resources res = ctx.getResources();
+		final Resources res = ctx.getResources();
 
-        mConnectingLineWeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                                                          connectingLineWeight,
-                                                          res.getDisplayMetrics());
+		mConnectingLineWeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, connectingLineWeight, res.getDisplayMetrics());
 
-        // Initialize the paint, set values
-        mPaint = new Paint();
-        mPaint.setColor(connectingLineColor);
-        mPaint.setStrokeWidth(mConnectingLineWeight);
-        mPaint.setAntiAlias(true);
+		// Initialize the paint, set values
+		mPaint = new Paint();
+		mPaint.setColor(connectingLineColor);
+		mPaint.setStrokeWidth(mConnectingLineWeight);
+		mPaint.setAntiAlias(true);
 
-        mY = y;
-    }
+		mY = y;
+	}
 
-    // Package-Private Methods /////////////////////////////////////////////////
+	// Package-Private Methods /////////////////////////////////////////////////
 
-    /**
-     * Draw the connecting line between the two thumbs.
-     * 
-     * @param canvas the Canvas to draw to
-     * @param leftThumb the left thumb
-     * @param rightThumb the right thumb
-     */
-    void draw(Canvas canvas, Thumb leftThumb, Thumb rightThumb) {
-        canvas.drawLine(leftThumb.getX(), mY, rightThumb.getX(), mY, mPaint);
-    }
+	/**
+	 * Draw the connecting line between the two thumbs.
+	 *
+	 * @param canvas
+	 * 		the Canvas to draw to
+	 * @param leftThumb
+	 * 		the left thumb
+	 * @param rightThumb
+	 * 		the right thumb
+	 */
+	void draw(Canvas canvas, Thumb leftThumb, Thumb rightThumb) {
+		canvas.drawLine(leftThumb.getX(), mY, rightThumb.getX(), mY, mPaint);
+	}
 }
--- rangebar/src/com/edmodo/rangebar/MeasureSpecMode.java
+++ rangebar/src/com/edmodo/rangebar/MeasureSpecMode.java
@@ -16,54 +16,53 @@ package com.edmodo.rangebar;
 import android.view.View;
 
 /**
- * Helper enum class for transforming a measureSpec mode integer value into a
- * human-readable String. The human-readable String is simply the name of the
- * enum value.
+ * Helper enum class for transforming a measureSpec mode integer value into a human-readable String. The human-readable String is simply the name
+ * of the enum value.
  */
 public enum MeasureSpecMode {
 
-    AT_MOST(View.MeasureSpec.AT_MOST),
-    EXACTLY(View.MeasureSpec.EXACTLY),
-    UNSPECIFIED(View.MeasureSpec.UNSPECIFIED);
+	AT_MOST(View.MeasureSpec.AT_MOST),
+	EXACTLY(View.MeasureSpec.EXACTLY),
+	UNSPECIFIED(View.MeasureSpec.UNSPECIFIED);
 
-    // Member Variables ////////////////////////////////////////////////////////
+	// Member Variables ////////////////////////////////////////////////////////
 
-    private final int mModeValue;
+	private final int mModeValue;
 
-    // Constructor /////////////////////////////////////////////////////////////
+	// Constructor /////////////////////////////////////////////////////////////
 
-    private MeasureSpecMode(int modeValue) {
-        mModeValue = modeValue;
-    }
+	private MeasureSpecMode(int modeValue) {
+		mModeValue = modeValue;
+	}
 
-    // Public Methods //////////////////////////////////////////////////////////
+	// Public Methods //////////////////////////////////////////////////////////
 
-    /**
-     * Gets the int value associated with this mode.
-     * 
-     * @return the int value associated with this mode
-     */
-    public int getModeValue() {
-        return mModeValue;
-    }
+	/**
+	 * Gets the int value associated with this mode.
+	 *
+	 * @return the int value associated with this mode
+	 */
+	public int getModeValue() {
+		return mModeValue;
+	}
 
-    /**
-     * Gets the MeasureSpecMode value that corresponds with the given
-     * measureSpec int value.
-     * 
-     * @param measureSpec the measure specification passed by the platform to
-     *            {@link View#onMeasure(int, int)}
-     * @return the MeasureSpecMode that matches with that measure spec
-     */
-    public static MeasureSpecMode getMode(int measureSpec) {
+	/**
+	 * Gets the MeasureSpecMode value that corresponds with the given measureSpec int value.
+	 *
+	 * @param measureSpec
+	 * 		the measure specification passed by the platform to {@link View#onMeasure(int, int)}
+	 *
+	 * @return the MeasureSpecMode that matches with that measure spec
+	 */
+	public static MeasureSpecMode getMode(int measureSpec) {
 
-        final int modeValue = View.MeasureSpec.getMode(measureSpec);
+		final int modeValue = View.MeasureSpec.getMode(measureSpec);
 
-        for (MeasureSpecMode mode : MeasureSpecMode.values()) {
-            if (mode.getModeValue() == modeValue) {
-                return mode;
-            }
-        }
-        return null;
-    }
+		for (MeasureSpecMode mode : MeasureSpecMode.values()) {
+			if (mode.getModeValue() == modeValue) {
+				return mode;
+			}
+		}
+		return null;
+	}
 }
